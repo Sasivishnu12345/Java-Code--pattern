6f34ccb695e30c7f4f8974067b1b611f21287e37
Sasivishnu12345 <sasivishnu19@gmail.com>
2025-07-11 09:36:48 +0530

    Create SpiralMatrix.java

---
+++ SpiralMatrix.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class SpiralMatrix{
+    public static void main(String[] args) {
+        int[][] arr = {
+            {1, 2, 3, 4}, 
+            {5, 6, 7, 8}, 
+            {9, 10, 11, 12}, 
+            {13, 14, 15, 16}
+        };
+
+        ArrayList<Integer> ls =new ArrayList<>();
+        int left=0,top=0;
+        int right=arr[0].length-1,bottom=arr.length-1;
+        while(left<=right && top<=bottom){
+            for(int i=left;i<=right;i++){
+                ls.add(arr[top][i]);
+            }
+            top++;
+             System.out.println(ls);
+            for(int i=top;i<=bottom;i++){
+                ls.add(arr[i][right]);
+            }
+            right--;
+             System.out.println(ls);
+            for(int i=right;i>=left;i--){
+                ls.add(arr[bottom][i]);
+            }
+            bottom--;
+             System.out.println(ls);
+            for(int i=bottom;i>=top;i--){
+                ls.add(arr[i][left]);
+            }
+            left++;
+             System.out.println(ls);
+        }
+       
+    }
+    
+}
